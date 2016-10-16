3aaedfb4397629c797bc9f91fb092055275fb533
Nick Triantafyllidis <ntrianta@lab.grid.auth.gr>
2016-10-16 10:54:08 +0200

    Initial Commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "github.com/garyburd/redigo/redis"
+)
+
+func main(){
+c, err := redis.Dial("tcp", "192.168.2.97:6379")
+if err != nil{
+  panic(err)
+}
+psc := redis.PubSubConn{c}
+psc.Subscribe("events")
+for {
+   switch n := psc.Receive().(type) {
+     case redis.Message:
+         fmt.Printf("Message: %s %s\n",n.Channel, n.Data)
+     case redis.PMessage:
+         fmt.Printf("PMessage: %s %s %s\n", n.Pattern, n.Channel, n.Data)
+     case redis.Subscription:
+         fmt.Printf("Subscription: %s %s %d\n", n.Kind, n.Channel, n.Count)
+         if n.Count == 0 {
+             return
+         }
+     case error:
+         fmt.Printf("error: %v\n", n)
+         return
+  }
+}
+}
